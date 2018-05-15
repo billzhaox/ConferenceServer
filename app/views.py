73c426affa0385e6cbec25cb04ce9fdb75d265b2
Becomebright <dsz6223042@buaa.edu.cn>
2018-05-15 19:06:46 +0800

    添加了base模板，使用BootStrap美化了页面

--- app/views.py
+++ app/views.py
@@ -5,19 +5,20 @@ from app import app
 from app.forms import AddConferenceForm
 from app.models import *
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     user = { 'username': 'Miguel' } # fake user
-#     return render_template("add_conference.html",
-#         title = 'Home',
-#         user = user)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'username': 'HHX'} # fake user
+    tag = {'name': 'index'}
+    return render_template("index.html", user=user, tag=tag)
 
 
 @app.route('/add_conference', methods = ['GET', 'POST'])
 def add_conference():
     form = AddConferenceForm()
-    user = {'username': 'HHX!'}
+    user = {'username': 'HHX'}
+    tag = {'name': 'add_conference'}
     if form.validate_on_submit():
         name = form.name.data
         date = form.date.data
@@ -25,4 +26,4 @@ def add_conference():
         conference = Conference(name=name, date=date, place=place)
         db.session.add(conference)
         db.session.commit()
-    return render_template('add_conference.html', user=user, form=form)
+    return render_template('add_conference.html', user=user, form=form, tag=tag)
